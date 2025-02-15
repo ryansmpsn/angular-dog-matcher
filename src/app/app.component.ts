4f91579d28c4f19517dedc8963e56a81975d508a
Ryan Simpson <ryzizn@gmail.com>
2025-02-14 21:38:56 -0500

    add counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { CounterComponent } from './components/counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
