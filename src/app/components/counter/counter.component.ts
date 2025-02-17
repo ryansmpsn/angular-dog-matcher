1bb258c0f42dd97904987627477ebe39c33aedcb
Ryan Simpson <ryzizn@gmail.com>
2025-02-17 10:52:22 -0500

    coutner component

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
+  count: WritableSignal<number> = signal(0);
+  time = signal(new Date().toLocaleTimeString());
+  title = input('Counter');
 
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(event);
+  }
+
+  increment() {
+    this.count.update((val) => val + 1);
+  }
+
+  decrement() {
+    this.count.update((val) => -1);
+  }
+
+  reset() {
+    this.count.set(0);
+  }
+
+  constructor() {
+    setInterval(() => {
+      this.time.set(new Date().toLocaleTimeString());
+    }, 1000);
+  }
 }
