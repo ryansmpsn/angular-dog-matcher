3a43c498ceb9e55b3a65e02a6d00bdd9520c5761
Ryan Simpson <ryzizn@gmail.com>
2025-02-17 10:52:01 -0500

    add layout component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormControl } from '@angular/forms';
-import { CounterComponent } from './components/counter/counter.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent],
+  imports: [LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
