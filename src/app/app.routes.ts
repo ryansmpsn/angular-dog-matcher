516e1e7c2aa557a1c08c740570832b2107a23892
Ryan Simpson <ryzizn@gmail.com>
2025-02-17 11:55:24 -0500

    set note on app router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,16 @@
 import { Routes } from '@angular/router';
+import { CounterComponent } from './components/counter/counter.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadComponent: () => {
-      return import('./components/counter/counter.component').then(
-        (m) => m.CounterComponent
-      );
-    }
+    component: CounterComponent
   },
   {
     path: 'todos',
     pathMatch: 'full',
+    // Examply of lazy loading a component. (Don't use by default! No premature optimizations!)
     loadComponent: () => {
       return import('./pages/todos/todos.component').then(
         (m) => m.TodosComponent
