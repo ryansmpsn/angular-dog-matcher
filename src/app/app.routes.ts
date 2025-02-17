334d0ba4bc5ffbdcc1e637d2f3bc88b2960d9546
Ryan Simpson <ryzizn@gmail.com>
2025-02-17 10:52:17 -0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./components/counter/counter.component').then(
+        (m) => m.CounterComponent
+      );
+    }
+  },
+  {
+    path: 'todos',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/todos/todos.component').then(
+        (m) => m.TodosComponent
+      );
+    }
+  }
+];
